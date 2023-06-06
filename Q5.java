30fc6a1f9c7c89b38840ef2aa9a04e4c57b6fa27
ANURAG PANDEY <52095894+Anurag15102000@users.noreply.github.com>
2023-06-07 00:05:51 +0530

    Update Q5.java

--- Q5.java
+++ Q5.java
@@ -17,7 +17,7 @@ public class Q5 {
     }  
        
     public static void main(String a[]){  
-        int[] arr1 = {9,14,3,2,43,11,58,22};  
+        int[] arr1 = {89,14,3,33,43,61,58,22};  
         System.out.println("Before Selection Sort");  
         for(int i:arr1){  
             System.out.print(i+" ");  
